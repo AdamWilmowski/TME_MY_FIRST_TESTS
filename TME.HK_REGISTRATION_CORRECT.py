78bc474a3f182b8f2da3ea595e9095cd21fe7cff
Adam Wilmowski <adam.wilmowski@gmail.com>
2023-07-19 22:14:28 +0200

    First works on proper registration test on tme.hk

---
+++ TME.HK_REGISTRATION_CORRECT.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import time
+import random
+import string
+import sqlite3
+import imaplib
+import email
+
+driver_service = Service(executable_path="C:\Webdriver\chromedriver.exe")
+driver = webdriver.Chrome(service=driver_service)
+
+sql = sqlite3.connect('TMEHK.db')
+cursor = sql.cursor()
+
+parameter = 'email'
+value = 1
+
+sql_insert_query = "INSERT INTO used_values (parameter, value) VALUES (?, ?)"
+
+cursor.execute(sql_insert_query, (parameter, value))
+sql.commit()
+sql.close()
+
+def get_random_value(length, type="string"):
+    if type == "string":
+        letters = string.ascii_letters
+        result_value = ''.join(random.choice(letters) for _ in range(length))
+    elif type == "number":
+        digits = string.digits
+        result_value = ''.join(random.choice(digits) for _ in range(length))
+    else:
+        raise ValueError("Invalid type. Use 'string' or 'number'.")
+
+    return result_value
+
+
+#driver.get('https://www.tme.hk/en/register')
+#time.sleep(1)
+
+#driver.find_element(By.ID, 'cookies-consent-close-icon').click()
+#time.sleep(1)
+
+field_1 = 'app_company_user_company_name'
+field_2 = 'app_company_user_company_email'
+field_3 = 'app_company_user_company_phoneNumber'
+field_4 = 'app_company_user_company_city'
+field_5 = 'app_company_user_company_street'
+field_6 = 'app_company_user_company_postcode'
+field_7 = 'app_company_user_jobTitle'
+field_8 = 'app_company_user_customer_email'
+field_9 = 'app_company_user_customer_phoneNumber'
+field_10 = 'app_company_user_customer_firstName'
+field_11 = 'app_company_user_customer_lastName'
+
+company_name = get_random_value(5, "string")
